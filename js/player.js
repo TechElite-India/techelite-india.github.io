6f40e14d9fa6048c0ad1d1060c52b5d3bfa9588b
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2020-09-23 15:59:37 +0530

    Changed course description links

--- js/player.js
+++ js/player.js
@@ -1,28 +1,24 @@
 jQuery(function() {
     $('#demoModal').on('hidden.bs.modal', function() {
-        console.log('f');
         $('#player').attr('src', "");
     });
 
     $('#demo1').on('click', function() {
-        console.log('demo1');
         $('#player').attr('src', "https://www.youtube.com/embed/EzJhTb564iA");
         $('#player').attr('height', $(window).height() * 0.6);
     });
 
     $('#demo2').on('click', function() {
-        console.log('demo2');
         $('#player').attr('src', "https://www.youtube.com/embed/EzJhTb564iA");
         $('#player').attr('height', $(window).height() * 0.6);
     });
 
     $('#demo3').on('click', function() {
-        console.log('demo3');
         $('#player').attr('src', "https://www.youtube.com/embed/EzJhTb564iA");
         $('#player').attr('height', $(window).height() * 0.6);
     });
-})
+});
 
 $(window).on('orientationchange', function() {
     // $('#player').attr('height', $('#demoModal').width() * 0.3);
-})
+});
