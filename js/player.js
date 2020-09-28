576ad0f50d18523a541dcd020f1ffca43d63047a
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2020-09-28 15:49:17 +0530

    Products section content added

--- js/player.js
+++ js/player.js
@@ -4,7 +4,7 @@ jQuery(function() {
     });
 
     $('#demo1').on('click', function() {
-        $('#player').attr('src', "https://www.youtube.com/embed/EzJhTb564iA");
+        $('#player').attr('src', "https://www.youtube.com/embed/tPgBUhI7N94");
         $('#player').attr('height', $(window).height() * 0.6);
     });
 
