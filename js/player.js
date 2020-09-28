1737843593d047823544206ee05b908a7c5cb8a1
Raghuvansh Raj <raghuvanshraj@hotmail.com>
2020-09-29 00:38:30 +0530

    Courses cards made consistent

--- js/player.js
+++ js/player.js
@@ -6,14 +6,12 @@ jQuery(function() {
     $('#demo1').on('click', function() {
         $('#player').attr('src', "https://www.youtube.com/embed/tPgBUhI7N94");
         $('#player').attr('height', $(window).height() * 0.6);
+        $('.modal-title').html('Personalized Coding Interviews: Demo');
     });
 
     $('#demo2').on('click', function() {
         $('#player').attr('src', "https://www.youtube.com/embed/cShJfwk8sfE");
         $('#player').attr('height', $(window).height() * 0.6);
+        $('.modal-title').html('Company-Wise Top Interview Questions: Demo');
     });
-});
-
-$(window).on('orientationchange', function() {
-    // $('#player').attr('height', $('#demoModal').width() * 0.3);
 });
